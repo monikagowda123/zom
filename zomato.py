2f14f718df1f84618c2c101810c68fb178fa7894
monikagowda <109779401+monikagowda123@users.noreply.github.com>
2022-08-19 11:53:38 +0530

    Update zomato.py

--- zomato.py
+++ zomato.py
@@ -2,7 +2,7 @@ username = input(' enter your username: ')
 if username == 'Put Your Username Here':#change this
    print("checking username")
    time.sleep(0.5)
-   print("username is right")
+   print("username is rightttttt")
    password = input(' type your password: ')
    if password == 'Put Your Password Here':#change this
     time.sleep(0.5)
